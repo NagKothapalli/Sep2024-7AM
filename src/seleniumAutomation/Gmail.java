dd32407173fb8507069891a16fc1b65445a48672
NagKothapalli <nag022@gmail.com>
2024-09-19 08:42:12 +0530

    Sep19th code pushed

--- src/seleniumAutomation/Gmail.java
+++ src/seleniumAutomation/Gmail.java
@@ -3,15 +3,22 @@ package seleniumAutomation;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import junit.framework.Assert;
+
 public class Gmail
 {
+	
+	String expectedTitle = "Gmail";  // Given by customer in the requirement document
+	
 	@Test
 	public void launchApplication()
 	{
 		System.out.println("RC : Launch Application");
 		//open an empty browser , call the url 
 		//System.setProperty("webdriver.chrome.driver", "path of chromedriver.exe");
+		
 		ChromeDriver driver = new ChromeDriver(); // chromedriver constructor will be executed as a result an empty chrome browser will be opened
+		
 		//Every browser window will have a unique session ID , selenium will identify the browser based on its session ID .
 		
 		driver.get("https://gmail.com");
@@ -20,6 +27,44 @@ public class Gmail
 		
 		System.out.println("My window session ID :"+ sessionID);
 		
+		String actualTitle = driver.getTitle(); // captured from the application
+		
+		//Validation = ExpectedValue : ActualValue
+		
+		//Junit Assertions
+		
+		Assert.assertEquals(expectedTitle, actualTitle);
+		
+		//User assertion
+		if(expectedTitle.equals(actualTitle))
+		{
+			//true
+			System.out.println("Title Validation Passed");
+		}
+		else
+		{
+			System.out.println("Title Validation Failed");
+		}
+		
+		
+		String currentUrl = driver.getCurrentUrl();
+		
+		System.out.println("My Current URL :" + currentUrl );
+		
+		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
