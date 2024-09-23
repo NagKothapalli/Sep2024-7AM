53269864c432c5bceb63f5caa7d24a3fed206495
NagKothapalli <nag022@gmail.com>
2024-09-23 08:50:21 +0530

    Sep23rd code pushed

--- src/seleniumAutomation/Gmail.java
+++ src/seleniumAutomation/Gmail.java
@@ -12,6 +12,8 @@ public class Gmail
 	
 	String expectedTitle = "Gmail";  // Given by customer in the requirement document
 	
+	ChromeDriver driver = new ChromeDriver(); // chromedriver constructor will be executed as a result an empty chrome browser will be opened
+	
 	@Test
 	public void launchApplication()
 	{
@@ -23,9 +25,9 @@ public class Gmail
 		
 		//System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\JarFiles\\chromedriver-win64-129\\chromedriver-win64\\chromedriver.exe");
 		
-		//ChromeDriver driver = new ChromeDriver(); // chromedriver constructor will be executed as a result an empty chrome browser will be opened
+		int stdNum; // local variable
 		
-		EdgeDriver driver = new EdgeDriver();
+		//EdgeDriver driver = new EdgeDriver();
 		
 		//Every browser window will have a unique session ID , selenium will identify the browser based on its session ID .
 		
@@ -58,6 +60,18 @@ public class Gmail
 		String currentUrl = driver.getCurrentUrl();
 		
 		System.out.println("My Current URL :" + currentUrl );
+		
+		
+	}
+	//org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {"method":"css selector","selector":"*[name='identifier']"}
+	//java.lang.IndexOutOfBoundsException: Index 2 out of bounds for length 0
+	@Test
+	public void loginToApplication()
+	{
+		System.out.println("Login to Application started ....");
+		launchApplication();
+		//System.out.println(stdNum);
+		//System.out.println(expectedTitle);
 		boolean flag = driver.findElement(By.name("identifier")).isDisplayed();
 		System.out.println(flag);
 		if(driver.findElement(By.name("identifier")).isEnabled())
@@ -66,18 +80,23 @@ public class Gmail
 			driver.findElement(By.name("identifier")).sendKeys("jfglgkfdlgmfdgmdf");
 			driver.findElement(By.name("identifier")).clear();
 			driver.findElement(By.name("identifier")).sendKeys("nag022@gmail.com");
+			driver.findElements(By.className("VfPpkd-vQzf8d")).get(1).click();
 		}
 		else
 		{
 			System.out.println("Object is not enabled");
 		}
-		
 	}
 	
-	
-	
-	
-	
+	@Test
+	public void creatAccount()
+	{
+		launchApplication();
+		System.out.println("Test Case : Verify the Create Account Functionality");
+		int count = driver.findElements(By.className("VfPpkd-vQzf8d")).size();
+		System.out.println("Total elements count :" + count);
+		driver.findElements(By.className("VfPpkd-vQzf8d")).get(2).click();  // 0  1  2 
+	}
 	
 	
 	
