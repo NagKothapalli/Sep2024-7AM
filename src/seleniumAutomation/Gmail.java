3bfc37505a96a5b3c805ece6a37b1cc4447a8211
NagKothapalli <nag022@gmail.com>
2024-09-26 08:43:41 +0530

    Code till 26th sep24 pushed

--- src/seleniumAutomation/Gmail.java
+++ src/seleniumAutomation/Gmail.java
@@ -2,6 +2,8 @@ package seleniumAutomation;
 
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -16,8 +18,13 @@ public class Gmail
 	String expectedTitle = "Gmail";  // Given by customer in the requirement document
 	
 	ChromeDriver driver = new ChromeDriver(); // chromedriver constructor will be executed as a result an empty chrome browser will be opened
-	
-	@Test
+	@After
+	public void logoutApplication()
+	{
+		System.out.println("Logout From Application");
+	}
+	//@Test
+	@Before
 	public void launchApplication()
 	{
 		System.out.println("RC : Launch Application");
@@ -72,7 +79,7 @@ public class Gmail
 	public void loginToApplication()
 	{
 		System.out.println("Login to Application started ....");
-		launchApplication();
+		//launchApplication();
 		//System.out.println(stdNum);
 		//System.out.println(expectedTitle);
 		boolean flag = driver.findElement(By.name("identifier")).isDisplayed();
@@ -94,15 +101,86 @@ public class Gmail
 	@Test
 	public void creatAccount()
 	{
-		launchApplication();
+		//launchApplication();
 		System.out.println("Test Case : Verify the Create Account Functionality");
 		int count = driver.findElements(By.className("VfPpkd-vQzf8d")).size();
 		System.out.println("Total elements count :" + count);
 		//driver.findElements(By.className("VfPpkd-vQzf8d")).get(2).click();  // 0  1  2 
-		List<WebElement> myElements = driver.findElements(By.className("VfPpkd-vQzf8d"));
-		WebElement element = myElements.get(2);
-		element.click();
+		List<WebElement> elements = driver.findElements(By.className("VfPpkd-vQzf8d"));
+		//WebElement element = elements.get(2);
+		//element.click();
+		
+		for(int i=0;i<elements.size();i++)  // i=0
+		{
+			String text = elements.get(i).getText();  // i th
+			if(text.equals("Create account"))
+			{
+				 elements.get(i).click();
+				 break;
+			}
+			
+		}
+		
 	}
+	
+	@Test
+	public void forgotEmail()
+	{
+		//launchApplication();
+		//driver.findElements(By.tagName("button")).get(0).click();
+		List<WebElement> elements = driver.findElements(By.tagName("button"));
+		
+		for(int i=0;i<elements.size();i++)  // i=0
+		{
+			String text = elements.get(i).getText();  // i th
+			if(text.equals("Forgot email?"))
+			{
+				 elements.get(i).click();
+				 break;
+			}
+			
+		}
+		
+	}
+	
+	@Test
+	public void learMoreAboutGuestMode()
+	{
+		//launchApplication();
+		//driver.findElements(By.tagName("button")).get(0).click();
+		List<WebElement> elements = driver.findElements(By.tagName("a"));
+		
+		for(int i=0;i<elements.size();i++)  // i=0
+		{
+			String text = elements.get(i).getText();  // i th
+			if(text.equals("Learn more about using Guest mode"))
+			{
+				 elements.get(i).click();
+				 break;
+			}
+			
+		}
+		
+	}
+	
+	
+	
+	//WebDriver : ChromeDriver : get(),getWindowHandle(),getTitle(),getCurrentUrl(),findElement(),findElements(),
+	
+	//By : id , name , class , tagName , linkText , partialLinkText , xpath , cssSelector
+	
+	//WebElement : click(),clear(),sendKeys(),isDisplayed(),isEnabled(),getText() , getTagName , getClassName , getAttribute ,findElement(),findElements()
+	
+	@Test
+	public void learMoreAboutGuestMode_linktext()
+	{
+		//launchApplication();
+		//driver.findElement(By.linkText("Learn more about using Guest mode")).click();
+		driver.findElement(By.partialLinkText("Learn more about using")).click();
+	}
+	
+	
+	
 	
 	
 	
