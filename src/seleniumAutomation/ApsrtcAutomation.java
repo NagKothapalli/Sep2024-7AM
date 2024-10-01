e2b565fcbf77fcdecdf6f00f2d40bc5d0a176e6d
NagKothapalli <nag022@gmail.com>
2024-10-01 10:46:20 +0530

    Oct1st code pushed

--- src/seleniumAutomation/ApsrtcAutomation.java
+++ src/seleniumAutomation/ApsrtcAutomation.java
@@ -1,9 +1,13 @@
 package seleniumAutomation;
 
+import java.time.Duration;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class ApsrtcAutomation 
 {	
@@ -26,12 +30,25 @@ public class ApsrtcAutomation
 	}
 	
 	
+	
 	@Test
-	public void bookBusTicket()
+	public void bookBusTicket() throws InterruptedException
 	{
 		System.out.println("TestCase : Book a Bus Ticket");
 		//driver.findElement(By.xpath("//input[@name='source']")).sendKeys("HYDERABAD");
 		driver.findElement(By.xpath("//input[@size='22' and @name='source']")).sendKeys("HYDERABAD");
+		Thread.sleep(1000);
+		Actions  actions = new Actions(driver); //1234
+		actions.sendKeys(Keys.ENTER).build().perform();
+		driver.findElement(By.xpath("//input[@id='searchBtn']")).click();
+		// driver can switch to another window , alert , iframe 
+		driver.switchTo().alert().accept();
+		driver.findElement(By.xpath("//input[@id='toPlaceName']")).sendKeys("GUNTUR");
+		actions.pause(Duration.ofSeconds(1)).sendKeys(Keys.ENTER).build().perform();
+		driver.findElement(By.xpath("//input[@id='searchBtn']")).click();
+		driver.findElement(By.xpath("//input[@id='txtJourneyDate']")).click();
+		driver.findElement(By.xpath("//a[text()='10']")).click();
+		driver.findElement(By.xpath("//input[@id='searchBtn']")).click();
 	}
 	
 	
